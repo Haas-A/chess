f19d57bf5a4faa3c21af1e91842761f041bea1eb
Alex <haas.411@osu.edu>
2021-11-19 14:13:19 -0500

    Env setting up

--- main.py
+++ main.py
@@ -1,1 +1,28 @@
- 
+import matplotlib.pyplot as plt
+import matplotlib.image as mp
+from pynput.mouse import Listener
+
+img = mp.imread('board.jpg')
+imgplot = plt.imshow(img)
+plt.show()
+
+def click(x, y, button):
+    print(x)
+    print(y)
+    print(button)
+
+
+def on_move(x, y):
+     print ("Mouse moved")
+    
+def on_click(x, y, button, pressed):
+    if pressed:
+        click(x, y, button)
+
+
+with Listener(on_move=on_move, on_click=on_click) as listener:
+    listener.join()
+
+
+
+
