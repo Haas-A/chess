c2105d6aa89b27a47188ed138e96b069ee13b1af
Alex <haas.411@osu.edu>
2021-11-24 01:43:05 -0500

    Chess board and pieces appear in a window.

---
+++ engine.py
@@ -0,0 +1,17 @@
+"""
+This will be the engine of the game, keeping the current state of the game.
+"""
+class GameState():
+    def __init__(self):
+        #board is 8x8 2d list, with 2 characters per element
+        self.board = [
+            ["bR", "bN", "bB", "bQ", "bK", "bB", "bN", "bR"],
+            ["bp", "bp", "bp", "bp", "bp", "bp", "bp", "bp"],
+            ["--", "--", "--", "--", "--", "--", "--", "--"],
+            ["--", "--", "--", "--", "--", "--", "--", "--"],
+            ["--", "--", "--", "--", "--", "--", "--", "--"],
+            ["--", "--", "--", "--", "--", "--", "--", "--"],
+            ["wp", "wp", "wp", "wp", "wp", "wp", "wp", "wp"],
+            ["wR", "wN", "wB", "wQ", "wK", "wB", "wN", "wR"]]
+        self.whiteMove = True
+        self.moveLog = []
--- main.py
+++ main.py
@@ -1,24 +1,53 @@
-mport matplotlib.pyplot as plt
-import matplotlib.image as mp
-from pynput.mouse import Listener
+import pygame as py
+# from chess import engine
+import engine
 
-img = mp.imread('board.jpg')
-imgplot = plt.imshow(img)
-plt.show()
+WIDTH = HEIGHT = 512
+DIMENSION = 8
+SQ_SIZE = HEIGHT // DIMENSION
+MAX_FPS = 15
+IMAGES = {}
 
-def click(x, y, button):
-        print(x)
-            print(y)
-                print(button)
+def loadImages():
+    pieces = ['wp', 'wR', 'wN', 'wB', 'wK', 'wQ', 'bp', 'bR', 'bN', 'bB', 'bK', 'bQ']
+    for piece in pieces:
+        IMAGES[piece] = py.image.load("images/" + piece + ".png")
+    
 
+def main():
+    py.init()
+    screen = py.display.set_mode((WIDTH, HEIGHT))
+    clock = py.time.Clock()
+    screen.fill(py.Color("white"))
+    gs = engine.GameState()
+    #print(gs.board) prints to terminal where pieces are
+    loadImages()
+    running = True
+    while running:
+        for e in py.event.get():
+            if e.type == py.QUIT:
+                running = False
+        drawGameState(screen, gs)
+        clock.tick(MAX_FPS)
+        py.display.flip()
 
-                def on_move(x, y):
-                         print ("Mouse moved")
-                             
-                             def on_click(x, y, button, pressed):
-                                     if pressed:
-                                                 click(x, y, button)
+def drawGameState(screen, gs):
+    drawBoard(screen)
+    drawPieces(screen, gs.board)
 
+def drawBoard(screen):
+    colors = [py.Color("white"), py.Color("gray")]
+    for r in range(DIMENSION):
+        for c in range(DIMENSION):
+            color = colors[((r+c) % 2)]
+            py.draw.rect(screen, color, py.Rect(c*SQ_SIZE, r*SQ_SIZE, SQ_SIZE, SQ_SIZE))
 
-                                                 with Listener(on_move=on_move, on_click=on_click) as listener:
-                                                         listener.join()
+def drawPieces(screen, board):
+    for r in range(DIMENSION):
+        for c in range(DIMENSION):
+            piece = board[r][c]
+            if piece != "--":
+                screen.blit(IMAGES[piece], py.Rect(c*SQ_SIZE, r*SQ_SIZE, SQ_SIZE, SQ_SIZE))
+
+if __name__ == "__main__":
+    main()
